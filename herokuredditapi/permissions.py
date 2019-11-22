0770f25c068383e63017bd47350774d607ef93e2
gruiz <gruiz@salesforce.com>
2019-11-22 17:06:07 -0300

    * Created permissions.py to have custom rest_framework.permissions Only a permission for OauthConfirm added for now
    * Created a class to expand TokenAuthentication from rest_framework
    Need to work in authenticate methods

---
+++ herokuredditapi/permissions.py
@@ -0,0 +1,36 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import exceptions
+from django.core.cache import cache
+
+from herokuredditapi.utils import utils
+logger = utils.init_logger(__name__)
+
+
+class MyOauthConfirmPermission(BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        # if request.method in SAFE_METHODS:
+        #     return True
+
+        logger.info(request.user)
+        state = request.query_params.get('state')
+        if not state:
+            raise exceptions.ParseError(
+                detail={'detail': 'state param not found'})
+        oauth_data = cache.get(f'oauth_{state}')
+        if not oauth_data:
+            raise exceptions.PermissionDenied(
+                detail={'detail': 'Invalid or expired state'})
+        else:
+            if request.method == 'POST' and oauth_data['status'] != 'accepted':
+                raise exceptions.PermissionDenied(
+                    detail={'detail': 'Authorization still pending.'})
+
+        # Write permissions are only allowed to the owner of the snippet.
+        # return obj.owner == request.user
+        return True
