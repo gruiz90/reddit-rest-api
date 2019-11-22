45546c4fcab752a99d2d917d478cc2a7f6f8cb3e
gruiz <gruiz@salesforce.com>
2019-11-22 16:56:37 -0300

    Redditor model and serializer

--- redditors/models.py
+++ redditors/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from herokuredditapi.modelmanager import MyModelManager
 
-# Create your models here.
+
+class Redditor(models.Model):
+    id = models.CharField(max_length=64, primary_key=True,
+                          help_text='The ID of the Redditor.')
+    name = models.CharField(
+        max_length=256, help_text='The Redditor’s username.', db_index=True)
+    created_utc = models.DateTimeField(
+        help_text='Time the account was created, represented in Unix Time.')
+    has_verified_email = models.BooleanField(
+        default=False, help_text='Whether or not the Redditor has verified their email.')
+    icon_img = models.URLField(help_text='The url of the Redditors’ avatar.')
+    comment_karma = models.IntegerField(
+        default=0, help_text='The comment karma for the Redditor.')
+    link_karma = models.IntegerField(
+        default=0, help_text='The link karma for the Redditor.')
+    is_employee = models.BooleanField(null=True,
+                                      help_text='Whether or not the Redditor is a Reddit employee.')
+    is_friend = models.BooleanField(
+        null=True, help_text='Whether or not the Redditor is friends with the authenticated user.')
+    is_mod = models.BooleanField(
+        null=True, help_text='Whether or not the Redditor mods any subreddits.')
+    is_gold = models.BooleanField(
+        null=True, help_text='Whether or not the Redditor has active gold status.')
+
+    objects = MyModelManager()
+
+    class Meta:
+        ordering = ['name', 'created_utc']
+
+    def __str__(self):
+        result = [f'Name: {self.name}', f'Created UTC: {self.created_utc}',
+                  f'Comment Karma: {self.comment_karma}', f'Link Karma: {self.link_karma}', ]
+        return ', '.join((str(x) for x in result))
---
+++ redditors/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Redditor
+
+
+class RedditorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Redditor
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Redditor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.has_verified_email = validated_data.get(
+            'has_verified_email', instance.has_verified_email)
+        instance.icon_img = validated_data.get('icon_img', instance.icon_img)
+        instance.comment_karma = validated_data.get(
+            'comment_karma', instance.comment_karma)
+        instance.link_karma = validated_data.get(
+            'link_karma', instance.link_karma)
+        instance.is_employee = validated_data.get(
+            'is_employee', instance.is_employee)
+        instance.is_friend = validated_data.get(
+            'is_friend', instance.is_friend)
+        instance.is_mod = validated_data.get('is_mod', instance.is_mod)
+        instance.is_gold = validated_data.get('is_gold', instance.is_gold)
+        instance.save()
+        return instance
