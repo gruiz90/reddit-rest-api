537fcbb4f122a6ac1a015ad357ebd57bae55f41b
gruiz <gruiz@salesforce.com>
2019-12-03 17:54:21 -0300

    * Added a many-to-many relation between ClientOrg and Subreddit models (migrations created) * Added a utils file for methods to use with subreddits * Created views to connect, disconnect, subscribe, unsubscribe, get rules, get details, from a Subreddit by the name, and get subscriptions for client. * Created the urls with the correct paths for all subreddits views.

--- subreddits/urls.py
+++ subreddits/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import SubredditViewSet
+from django.urls import path
+import subreddits.views as views
 
 app_name = 'subreddits'
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register('subreddits', SubredditViewSet)
-
-urlpatterns = router.urls
+urlpatterns = [
+	path('', views.SubredditSubscriptions.as_view()),
+	path('<str:name>', views.SubredditView.as_view()),
+	path('<str:name>/connect', views.ConnectSubreddit.as_view()),
+	path('<str:name>/disconnect', views.DisconnectSubreddit.as_view()),
+	path('<str:name>/rules', views.SubredditRules.as_view()),
+	path('<str:name>/subscribe', views.SubredditSubscribe.as_view()),
+	path('<str:name>/unsubscribe', views.SubredditUnsubscribe.as_view()),
+]
--- subreddits/views.py
+++ subreddits/views.py
@@ -1,52 +1,212 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+#!/usr/bin/env python3
 from .models import Subreddit
 from .serializers import SubredditSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
-from rest_framework.exceptions import NotFound
-import json
+from rest_framework import status, exceptions
+from rest_framework.permissions import IsAuthenticated
+from herokuredditapi.tokenauthentication import MyTokenAuthentication
+from .utils import sub_utils
 
-# Create your views here.2
+from herokuredditapi.utils import utils
+logger = utils.init_logger(__name__)
 
 
-class SubredditViewSet(viewsets.ModelViewSet):
+class ConnectSubreddit(APIView):
     """
-    API endpoint that allows subreddits to be viewed or edited.
+    API endpoint to connects a Salesforce org client to a subreddit by the name.
+    This creates a connection between the ClientOrg and Subreddit, subscribes the reddit user if not already
+    and returns all the relevant data about the subreddit.
     """
-    queryset = Subreddit.objects.all().order_by('created_utc')
-    serializer_class = SubredditSerializer
 
-    def create(self):
-        # subredditNameToGet = self.request.query_params.get('name')
-        # if subredditNameToGet is not None:
-        # 	subreddit = Subreddit.get
-        # 	subreddit = Subreddit.get_or_create_by_name(subredditNameToGet)
-        # 	if subreddit is None:
-        # 		raise NotFound(detail="Error 404, subreddit not found", code=404)
-        # 	return Response(201, SubredditSerializer(subreddit).data)
-        # else:
-        # 	pass
-        pass
+    authentication_classes = [MyTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, name, Format=None):
+        """POST request that expects the subreddit name to connect the client to.
+        Arguments:
+                request {[type]} -- DRF Request object
+                name {[string]} -- Subreddit name
+        Keyword Arguments:
+                Format {[string]} -- [description] (default: {None})
+        """
+        logger.info('-' * 100)
+        logger.info('New connect subreddit request...')
+
+        # Gets the reddit instance from the user in request (ClientOrg)
+        client_org = request.user
+        reddit = utils.new_client_request(client_org)
+        # Get subreddit instance with the name provided
+        subreddit = reddit.subreddit(name)
+
+        logger.info(f'Connecting to Subreddit {subreddit.name}')
+        if not subreddit.user_is_subscriber:
+            logger.info(
+                'Client is not subscribed, so I need to subscribe him here..')
+            subreddit.subscribe()
+        logger.info(f'Client subscribed: {subreddit.user_is_subscriber}')
+
+        # Get data I need from subreddit instance
+        subreddit_data = sub_utils.get_subreddit_data(subreddit)
+
+        # Here for now I'll just save the subreddit_data in a Subreddit Model object
+        # But I need to enqueue this into a redis queue, send the data to user as fast as possible
+        subreddit_obj = sub_utils.create_or_update(subreddit_data)
+        # Add the client_org connection to the object
+        subreddit_obj.clients.add(client_org)
+        # Show info about the subreddit obj in the log
+        logger.info(repr(subreddit_obj))
+
+        return Response(subreddit_data, status=status.HTTP_200_OK)
 
 
-# class SubredditAPIView(APIView):
-# 	"""
-# 	View to list all users in the system.
+class DisconnectSubreddit(APIView):
+    """
+    API endpoint to disconnect a Salesforce org client to a Subreddit by the name.
+    This only removes the connection between the ClientOrg and the Subreddit if exists.
+    """
 
-# 	* Requires token authentication.
-# 	* Only admin users are able to access this view.
-# 	"""
-# 	# authentication_classes = [authentication.TokenAuthentication]
-# 	# permission_classes = [permissions.IsAdminUser]
+    authentication_classes = [MyTokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-# 	def get(self):
-# 		"""
-# 		Return a list of all users.
-# 		"""
-# 		subredditNameToGet = self.request.query_params.get('name')
-# 		subreddit = Subreddit.get_by_name(subredditNameToGet)
-# 		if subreddit is None:
-# 			raise NotFound(detail="Error 404, subreddit not found", code=404)
-# 		return Response(json.dumps(subreddit))
+    def post(self, request, name, Format=None):
+        logger.info('-' * 100)
+        logger.info('New disconnect subreddit request...')
+
+        # Gets the reddit instance from the user in request (ClientOrg)
+        client_org = request.user
+        reddit = utils.new_client_request(client_org)
+        # Get subreddit instance with the name provided
+        subreddit = reddit.subreddit(name)
+
+        subreddit_obj = Subreddit.objects.get_or_none(id=subreddit.id)
+        if subreddit_obj is None:
+            raise exceptions.NotFound(
+                detail={'detail': 'No subreddit found with the name provided to make the disconnection.'})
+        # Remove the client connection from both sides
+        subreddit_obj.clients.remove(client_org)
+        client_org.subreddit_set.remove(subreddit_obj)
+
+        return Response(data={'detail': 'Client disconnected subreddit succesfully.'},
+                        status=status.HTTP_200_OK)
+
+
+class SubredditView(APIView):
+    """
+    API endpoint to get the Subreddit data by the name provided.
+    """
+
+    authentication_classes = [MyTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, name, Format=None):
+        logger.info('-' * 100)
+        logger.info('New subreddit details request...')
+
+        # Gets the reddit instance from the user in request (ClientOrg)
+        client_org = request.user
+        reddit = utils.new_client_request(client_org)
+        # Get subreddit instance with the name provided
+        subreddit = reddit.subreddit(name)
+
+        # Get data I need from subreddit instance
+        subreddit_data = sub_utils.get_subreddit_data(subreddit)
+        # Enqueue this in a redis queue job later
+        sub_utils.create_or_update(subreddit_data)
+
+        return Response(subreddit_data, status=status.HTTP_200_OK)
+
+
+class SubredditSubscriptions(APIView):
+    """
+    API endpoint to get a list of subreddits subscriptions for the client.
+    """
+
+    authentication_classes = [MyTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, Format=None):
+        logger.info('-' * 100)
+        logger.info('New subreddit subscriptions details request...')
+
+        # Gets the reddit instance from the user in request (ClientOrg)
+        client_org = request.user
+        reddit = utils.new_client_request(client_org)
+
+        reddit_user = reddit.user
+        subreddits = []
+        for sub in reddit_user.subreddits():
+            subreddits.append(sub_utils.get_subreddit_data_simple(sub))
+
+        return Response({'subscriptions': subreddits}, status=status.HTTP_200_OK)
+
+
+class SubredditRules(APIView):
+    """
+    API endpoint to get the rules of a subreddit by name.
+    """
+
+    authentication_classes = [MyTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, name, Format=None):
+        logger.info('-' * 100)
+        logger.info('New subreddit get rules request...')
+
+        # Gets the reddit instance from the user in request (ClientOrg)
+        client_org = request.user
+        reddit = utils.new_client_request(client_org)
+        # Get subreddit instance with the name provided
+        subreddit = reddit.subreddit(name)
+
+        return Response(subreddit.rules(), status=status.HTTP_200_OK)
+
+
+class SubredditSubscribe(APIView):
+    """
+    API endpoint to subscribe a subreddit by the name
+    """
+
+    authentication_classes = [MyTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, name, Format=None):
+        logger.info('-' * 100)
+        logger.info('New subreddit subscribe request...')
+
+        # Gets the reddit instance from the user in request (ClientOrg)
+        client_org = request.user
+        reddit = utils.new_client_request(client_org)
+        # Get subreddit instance with the name provided
+        subreddit = reddit.subreddit(name)
+        if not subreddit.user_is_subscriber:
+            subreddit.subscribe()
+
+        logger.info(f'Client subscribed: {subreddit.user_is_subscriber}')
+
+        return Response({'detail': f'Client succesfully subscribed to {name}.'}, status=status.HTTP_200_OK)
+
+
+class SubredditUnsubscribe(APIView):
+    """
+    API endpoint to unsubscribe a subreddit by the name
+    """
+
+    authentication_classes = [MyTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, name, Format=None):
+        logger.info('-' * 100)
+        logger.info('New subreddit unsubscribe request...')
+
+        # Gets the reddit instance from the user in request (ClientOrg)
+        client_org = request.user
+        reddit = utils.new_client_request(client_org)
+        # Get subreddit instance with the name provided
+        subreddit = reddit.subreddit(name)
+        if subreddit.user_is_subscriber:
+            subreddit.unsubscribe()
+
+        logger.info(f'Client subscribed: {subreddit.user_is_subscriber}')
+
+        return Response({'detail': f'Client succesfully unsubscribed from {name}.'}, status=status.HTTP_200_OK)
