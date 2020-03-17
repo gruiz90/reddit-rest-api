ebb50a2279bda52a69f495a75f012759b6238b81
gruiz <gruiz@salesforce.com>
2020-03-17 16:10:10 -0300

    Small change in settings. Need database password in localhost as well as an env var.

--- api/settings.py
+++ api/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('POSTGRES_DATABASE_NAME', 'django_reddit_api'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': '',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },
